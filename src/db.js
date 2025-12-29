d9e0aa88acd429330ae398f478921b989b2b9555
vishnuvarthan <rvishnuxvarthan@gmail.com>
2025-12-29 20:54:04 +0530

    commit

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Vishnu@#$varthan',
+  password: '',
   database: process.env.DB_NAME || 'pastebin_lite',
 });
 
